3cc8ebc3b44970c2f8ce14e22878d40b9419f3cc
lawrenzevaldez <valdezlawrenze@gmail.com>
2026-03-18 17:41:39 +0800

    Add pm2 production

---
+++ process.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "srs_bo_supplier_updater",
+      script: "./server.js",
+      watch: ["start", "config", "app", "resources"],
+      ignore_watch: ["node_modules", "tmp", "public", "server.js"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
